2b32739c0019fa9066adafb11618ed23b207273b
obiogodwin5-hash <obiogodwin5@gmail.com>
2025-12-01 10:04:17 +0100

    update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
